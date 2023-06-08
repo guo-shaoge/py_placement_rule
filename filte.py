80173135a04e3ffeeb185aa28cca1b9162308f84
guo-shaoge <shaoge1994@163.com>
2023-06-08 13:29:48 +0800

    rename
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- filte.py
+++ filte.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as f:
     rules = json.load(f)
     new_rules = []
     for rule in rules:
-        if rule['id'].startswith('keyspace-5794'):
+        if rule['id'].startswith('keyspace-' + sys.argv[2]):
             # check and update group_id
             new_rules.append(rule)
     print(json.dumps(new_rules, indent=2))
